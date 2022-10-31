a2c41f8c821f58561aa3834e92193cac5732fdc9
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-10-31 10:52:59 +0100

    Dependencia de Black Blood V1.4

---
+++ version.js
@@ -0,0 +1,5 @@
+// ! Script version de Black Blood que se mostrará en el menú principal
+let version = 'Black Blood V1.4';
+
+let versionElement = document.getElementById('version');
+versionElement.innerHTML = version;
