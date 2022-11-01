645947661911fd33851affc17928cc24a2f0aa5e
navarro41yt <anavarro41yt@gmail.com>
2022-11-01 13:31:41 +0100

    BB v1.5.1

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 // ! Script version de BB que se mostrará en el menú principal
-let version = 'v1.5';
+let version = 'v1.5.1';
 
 let versionElement = document.getElementById('version');
 versionElement.innerHTML = version;
