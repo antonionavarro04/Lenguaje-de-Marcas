0c5d5911bf2f380bccee613aa8e33ad33d509795
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-11-07 09:11:23 +0100

    v1.1

--- js/randomChars/js/char.js
+++ js/randomChars/js/char.js
@@ -1,7 +1,7 @@
 let num = 0;
 
 // ! Declaramos un array con algunos caracteres
-let chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`~!@#$%^&*()_+-=[]{}|;':,./<>?";
+let chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`~!@#$%^&*()_+-=[]{}|;':,./<>?ºª€¬¿¡";
 
 // ! Declaramos una variable que contendra el texto que se mostrara en pantalla
 numJs = document.getElementById("num");
@@ -12,10 +12,19 @@ let random = Math.floor(Math.random() * chars.length);
 // ! Mostramos el numero aleatorio
 numJs.innerHTML = chars[random];
 
+// ! Sacamos el titulo actual del documento
+title = document.title;
+
+// ! Actualizamos el titulo con el numero
+document.title = title + " - [ " + chars[random] + " ]";
+
 setInterval(() => {
     // ? Generamos un numero aleatorio entre 0 y n
     let random = Math.floor(Math.random() * chars.length);
 
     // ! Mostramos el numero aleatorio
     numJs.innerHTML = chars[random];
+
+    // ! Actualizamos el titulo con el numero
+    document.title = title + " - [ " + chars[random] + " ]";
 }, 1000);
