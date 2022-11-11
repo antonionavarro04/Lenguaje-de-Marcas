c3fb530c73632579674747378f07670d443f5114
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-11-11 12:47:42 +0100

    bb v1.6.7

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 // ! Script version de BB que se mostrará en el menú principal
-const version = 'v1.6.6';
+const version = 'v1.6.7';
 const title = "LM - Index";
 
 var versionElement = document.getElementById('version');
