cf9d0ac2466e06ad1912bd504b7dc52dce1dce50
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-11-17 14:41:32 +0100

    moderLoginSample a.v.1.3

--- js/modernLoginSample/js/version.js
+++ js/modernLoginSample/js/version.js
@@ -1,9 +1,39 @@
-const version = "v1.2"
-const state = "alpha"
+const version = "v1.3";
+const state = "alpha";
 
-console.info("Version: " + version)
+console.info("Version: " + version);
+
+// ! Importaciones de Variables de HTML
+var htmlV = document.getElementById("version");
+var htmlS = document.getElementById("state");
 
 // ? Solo lo imprimiremos si el estado es diferente a la release
-if (state != "release") {
-    console.info("State: " + state)
+if (state != "stable" || state != "Stable") {
+    console.info("State: " + state);
+
+    // ! Imprimimos en HTML
+    htmlV.innerHTML = version;
+    htmlS.innerHTML = state;
+
+    switch (state) {
+        case "pre-alpha":
+            htmlS.style.color = "#990000";
+            break;
+        case "alpha":
+            htmlS.style.color = "red";
+            break;
+        case "beta":
+            htmlS.style.color = "blue";
+            break;
+        case "release":
+            htmlS.style.color = "green";
+            break;
+        default:
+            htmlS.style.color = "black";
+            break;
+    }
+
+} else {
+    // ! Imprimimos en HTML
+    htmlV.innerHTML = version;
 }
