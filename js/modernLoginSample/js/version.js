6490f7db1c0c2a68236110f751a328ec46670774
navarro41yt <anavarro41yt@gmail.com>
2022-11-20 12:32:01 +0100

    moderLoginSample b.v.1.1

--- js/modernLoginSample/js/version.js
+++ js/modernLoginSample/js/version.js
@@ -1,54 +1,61 @@
-const version = "v1.0";
-const state = "beta";
 
-console.info("Version: " + version);
+/**
+ * @author Antonio Navarro
+*/
 
-// ! Importaciones de Variables de HTML
+// ^ Definimos variables
+const version = "v1.1";
+const state = "Beta";
+
+// ^ Importaciones de Variables de HTML
 var htmlV = document.getElementById("version");
 var htmlS = document.getElementById("state");
 
-// ? Solo lo imprimiremos si el estado es diferente a la release
-if (state != "stable" && state != "Stable") {
-    console.info("State: " + state);
+// ! Imprimimos la Versión y el Estado
+console.info("Version: " + version);
+console.info("State: " + state);
 
-    // ! Imprimimos en HTML
-    htmlV.innerHTML = version;
-    htmlS.innerHTML = state;
+// ! Imprimimos en HTML la Versión y el Estado
+htmlV.innerHTML = version;
+htmlS.innerHTML = state;
 
-    switch (state) {
-        case "pre-alpha":
-            htmlS.style.color = "#990000";
-            console.error("This version is a pre-alpha, it may contain errors, missing content and is not recommended to use it.");
-            break;
-        case "alpha":
-            htmlS.style.color = "red";
-            console.warn("This phase is not stable yet, it may have errors.");
-            break;
-        case "beta":
-            htmlS.style.color = "blue";
-            console.warn("This phase is not stable yet, it may have errors.");
-            break;
-        case "release":
-            htmlS.style.color = "green";
-            console.warn("This phase is nearly stable but it may have some errors.");
-            break;
-        case "stable":
-            htmlS.remove();
-            break;
-        default:
-            htmlS.style.color = "orange";
-            htmlS.innerHTML = state + "?";
-            console.error("Esta versión no tiene un estado establecido.");
-            break;
-    }
-
-} else {
-    // ! Imprimimos en HTML
-    htmlV.innerHTML = version;
+// ! Imprimiremos un mensaje extra en la consola según el State y el color cambiara segun el State, si es Stable se ocultaran las versiones del Documento
+switch (state) {
+    case "Pre-Alpha":
+        htmlS.style.color = "#990000";
+        htmlV.style.color = "#990000";
+        console.error("This version is a pre-alpha, it may contain errors, missing content and is not recommended to use it.");
+        break;
+    case "Alpha":
+        htmlS.style.color = "red";
+        htmlV.style.color = "red";
+        console.warn("This phase isn't stable yet, it may have errors.");
+        break;
+    case "Beta":
+        htmlS.style.color = "blue";
+        htmlV.style.color = "blue";
+        console.warn("This phase isn't stable yet, it may have some errors.");
+        break;
+    case "Release":
+        htmlS.style.color = "green";
+        htmlV.style.color = "green";
+        console.warn("This phase is nearly stable but it may have small errors.");
+        break;
+    case "Stable":
+        htmlV.remove();
+        htmlS.remove();
+        break;
+    default:
+        htmlS.style.color = "orange";
+        htmlV.style.color = "orange";
+        htmlS.innerHTML = state + "?";
+        console.error("This version doesn't have a established state. It may be an error.");
+        break;
 }
 
 console.log("");
 
-console.info("Autor: Antonio Navarro");
-console.info("Github: https://github.com/antonionavarro04");
-console.info("Youtube: https://www.youtube.com/c/navarro41yt");
+// ^ Informaciones Extra y Spam
+console.error("Author: Antonio Navarro");
+console.error("Youtube: https://www.youtube.com/c/navarro41yt");
+console.error("Github: https://github.com/antonionavarro04");
