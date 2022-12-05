dbd53cf5b29c66f953ce64c0b0f8d5ef585ceb9c
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-12-05 13:44:51 +0100

    Todo

--- js/cards/js/script.js
+++ js/cards/js/script.js
@@ -6,6 +6,17 @@ let card1 = document.getElementById("card1");
 let card2 = document.getElementById("card2");
 let card3 = document.getElementById("card3");
 
-anime({
+var animation = anime({
     targets: card1,
+    translateX: 250,
+    backgroundColor: "#F11",
+    easing: "linear",
+    duration: 3000,
+    loop: true,
+});
+
+// Hacemos que al clickar actionTrigegr
+
+actionTrigger.addEventListener("click", function () {
+    animation.restart();
 });
